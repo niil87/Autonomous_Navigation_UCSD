06bdefff9e85fe8da1c3f1bf2e45d3110e1f7ab3
niil87 <challanikhil@gmail.com>
2019-08-26 02:43:13 -0700

    new files with mutex type protection

---
+++ pythonScripts/MySQL_Control.py
@@ -0,0 +1,42 @@
+import mysql.connector
+import time
+
+tries = 10
+while tries > 0 :
+    tries = tries - 1
+    try : 
+        connection = mysql.connector.connect(host='localhost', database='UCSDrobocar04_DataBase', user = 'control_process', password='team4ucsd')
+    except mysql.connector.errors.ProgrammingError:
+        if tries == 0:
+            print ("Failed to connect even after retrying " + str(tries) + " times")
+            break;    
+        else :
+            print ("retrying after 1 secs")
+            time.sleep(1)
+    else:
+        break
+
+cursor = connection.cursor()
+
+tries = 10
+while tries > 0 :
+    tries = tries - 1
+    try : 
+        cursor.execute('DESCRIBE PriceList;')
+    except mysql.connector.errors.ProgrammingError:
+        if tries == 0:
+            print ("Failed to connect even after retrying " + str(tries) + " times")
+            break;    
+        else :
+            print ("retrying after 1 secs")
+            time.sleep(1)
+    else:
+        break
+record = cursor.fetchall()
+print(record)
+cursor.execute('INSERT INTO PriceList VALUES (\'P1\',1.0)')
+connection.commit()
+a = input("Enter anything to continue")
+cursor.close()
+connection.close()
+print("Done!!")
