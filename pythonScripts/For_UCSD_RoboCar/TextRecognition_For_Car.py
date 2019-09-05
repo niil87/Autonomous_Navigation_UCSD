a04b1641d5455fccd22ab9604a2728c81a80f60a
niil87 <challanikhil@gmail.com>
2019-09-04 19:31:14 -0700

    change input/output

--- pythonScripts/For_UCSD_RoboCar/TextRecognition_For_Car.py
+++ pythonScripts/For_UCSD_RoboCar/TextRecognition_For_Car.py
@@ -9,63 +9,72 @@ import pytesseract
 import cv2
 
 def decode_predictions(scores, geometry):
-	# grab the number of rows and columns from the scores volume, then
-	# initialize our set of bounding box rectangles and corresponding
-	# confidence scores
-	(numRows, numCols) = scores.shape[2:4]
-	rects = []
-	confidences = []
+    min_confidence = 0.5
+    # grab the number of rows and columns from the scores volume, then
+    # initialize our set of bounding box rectangles and corresponding
+    # confidence scores
+    (numRows, numCols) = scores.shape[2:4]
+    rects = []
+    confidences = []
 
-	# loop over the number of rows
-	for y in range(0, numRows):
-		# extract the scores (probabilities), followed by the
-		# geometrical data used to derive potential bounding box
-		# coordinates that surround text
-		scoresData = scores[0, 0, y]
-		xData0 = geometry[0, 0, y]
-		xData1 = geometry[0, 1, y]
-		xData2 = geometry[0, 2, y]
-		xData3 = geometry[0, 3, y]
-		anglesData = geometry[0, 4, y]
+    # loop over the number of rows
+    for y in range(0, numRows):
+        # extract the scores (probabilities), followed by the
+        # geometrical data used to derive potential bounding box
+        # coordinates that surround text
+        scoresData = scores[0, 0, y]
+        xData0 = geometry[0, 0, y]
+        xData1 = geometry[0, 1, y]
+        xData2 = geometry[0, 2, y]
+        xData3 = geometry[0, 3, y]
+        anglesData = geometry[0, 4, y]
 
-		# loop over the number of columns
-		for x in range(0, numCols):
-			# if our score does not have sufficient probability,
-			# ignore it
-			if scoresData[x] < min_confidence:
-				continue
+        # loop over the number of columns
+        for x in range(0, numCols):
+            # if our score does not have sufficient probability,
+            # ignore it
+            if scoresData[x] < min_confidence:
+                continue
 
-			# compute the offset factor as our resulting feature
-			# maps will be 4x smaller than the input image
-			(offsetX, offsetY) = (x * 4.0, y * 4.0)
+            # compute the offset factor as our resulting feature
+            # maps will be 4x smaller than the input image
+            (offsetX, offsetY) = (x * 4.0, y * 4.0)
 
-			# extract the rotation angle for the prediction and
-			# then compute the sin and cosine
-			angle = anglesData[x]
-			cos = np.cos(angle)
-			sin = np.sin(angle)
+            # extract the rotation angle for the prediction and
+            # then compute the sin and cosine
+            angle = anglesData[x]
+            cos = np.cos(angle)
+            sin = np.sin(angle)
 
-			# use the geometry volume to derive the width and height
-			# of the bounding box
-			h = xData0[x] + xData2[x]
-			w = xData1[x] + xData3[x]
+            # use the geometry volume to derive the width and height
+            # of the bounding box
+            h = xData0[x] + xData2[x]
+            w = xData1[x] + xData3[x]
 
-			# compute both the starting and ending (x, y)-coordinates
-			# for the text prediction bounding box
-			endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
-			endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
-			startX = int(endX - w)
-			startY = int(endY - h)
+            # compute both the starting and ending (x, y)-coordinates
+            # for the text prediction bounding box
+            endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
+            endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
+            startX = int(endX - w)
+            startY = int(endY - h)
 
-			# add the bounding box coordinates and probability score
-			# to our respective lists
-			rects.append((startX, startY, endX, endY))
-			confidences.append(scoresData[x])
+            # add the bounding box coordinates and probability score
+            # to our respective lists
+            rects.append((startX, startY, endX, endY))
+            confidences.append(scoresData[x])
 
-	# return a tuple of the bounding boxes and associated confidences
-	return (rects, confidences)
+    # return a tuple of the bounding boxes and associated confidences
+    return (rects, confidences)
 
 def MainTextRecognition():
+    # Hard coding values for UCSD Run #
+    eastModel = 'frozen_east_text_detection.pb'
+    imgW = 320
+    imgH = 320
+    min_confidence = 0.5
+    padding = 0.0
+    ####################################
+
     # load the input image and grab the image dimensions
     image = cv2.imread(img)
     orig = image.copy()
@@ -104,7 +113,9 @@ def MainTextRecognition():
 
     # initialize the list of results
     results = []
-
+    text = 0
+    CenterX = 0.0
+    CenterY = 0.0
     # loop over the bounding boxes
     for (startX, startY, endX, endY) in boxes:
         # scale the bounding box coordinates based on the respective
@@ -142,19 +153,19 @@ def MainTextRecognition():
         results.append(((startX, startY, endX, endY), text))
         CenterX = (startX + endX)/2
         CenterY = (startY + endY)/2
-        print("Text detected is: ", text)
-        print ("Text location is: " + str(CenterX) + ' ' + str(CenterY))
-        return (text,CenterX,CenterY)
+        if text == 'ONE' :
+            text = 1
+        elif text == 'TWO' :
+            text = 2
+        elif text == 'THREE' : 
+            text = 3
+        else :
+            text = 0
+        return (text)
 
-    # sort the results bounding box coordinates from top to bottom
-    results = sorted(results, key=lambda r:r[0][1])
-
+    return (text)
 
 img = 'THREE.jpg'
-eastModel = 'frozen_east_text_detection.pb'
-imgW = 320
-imgH = 320
-min_confidence = 0.5
-padding = 0.0
-(Str,X,Y) = MainTextRecognition()
 
+(Str) = MainTextRecognition()
+print("Text detected is: ", Str)
